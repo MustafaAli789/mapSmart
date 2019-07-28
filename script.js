c320a7e3632e88e45d53db935ef3951c414484ea
Mustafa Ali <alimustafa0987@gmail.com>
2019-07-28 16:22:05 -0400

    getting users location and centering map on it

--- script.js
+++ script.js
@@ -4,4 +4,29 @@ function createMap(){
 		center: {lat: 45.4215, lng: -75.6972},
 		zoom: 10
 	})
+
+	//Utilizing HTML5 Geolocation to get users current location
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(pos){
+			var usersPos = {
+				lat: pos.coords.latitude,
+				lng: pos.coords.longitude
+			};
+			map.panTo(usersPos);
+		}, function(){
+			//User has denied location access
+			handleLocationError(true, map.getCenter());
+		});
+	} else{
+		//Browser doesnt support the geolocation feature
+		handleLocationError(false, map.getCenter());
+	}
+}
+
+function handleLocationError(gelocationInBrowser, position){
+	if(geolocationInBrowser)
+		console.log("User has denied location access, centering map on Ottawa.");
+	else
+		console.log("Browser does not support gelocation service. Centering map on Ottawa");
+	map.panTo(pos);
 }
